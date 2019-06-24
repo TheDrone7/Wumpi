243a06263e42876d1b85ac09f0f8a20d2c76dbae
AlexWitkowski0 <alexwitkowski@outlook.com>
2019-06-24 16:11:44 -0400

    Basic command handler.

--- src/tasks/maintenance.js
+++ src/tasks/maintenance.js
@@ -1,7 +1,61 @@
+const Discord = require("discord.js");
+
 module.exports = {
     name: 'maintenance',
     description: 'Lock down the server for maintenance.',
+    guildOnly: true,
+    cooldown: 25,
+    aliases: ['lockdown'],
+    args: true,
+    usage: '"Reason"',
     execute(message, args) {
-        message.channel.send('Temporary test for making sure handler is all good.');
-    },
+        let server = message.channel.guild;
+        let lockdownChannel = server.channels.find(c => c.name.toString() === "maintenance" && c.type === "text");
+        if(!lockdownChannel){
+            /* Fallback maintenance message, in case it isn't set. **/
+            let maintenanceMessage =
+                'Default maintenance message.' +
+                '\nETA: 30 minutes';
+            if(args.length){
+                maintenanceMessage = args;
+            }
+            let maintenanceEmbed = new Discord.RichEmbed()
+                .setTimestamp()
+                .setColor(0xfc5c50)
+                .setTitle('Maintenance Mode')
+                .setDescription("The server is now in maintenance mode. You\'ll be able to access your channels soon."
+                    + "\n\nReason: "
+                    + maintenanceMessage)
+                .setFooter(message.author.username);
+
+            server.createChannel('maintenance', 'text',[
+                {
+                    id: server.defaultRole.id,
+                    allow: ['VIEW_CHANNEL'],
+                    deny: ['SEND_MESSAGES'],
+                }
+            ]).then(channel => {
+                channel.send(maintenanceEmbed);
+                channel.overwritePermissions(server.defaultRole.id, { VIEW_CHANNEL: true });
+                channel.overwritePermissions(server.defaultRole.id, { SEND_MESSAGES: false });
+            });
+            let channels = message.guild.channels;
+            channels.forEach(c => {
+                if(c.permissionOverwrites){
+
+                }else{
+                    c.overwritePermissions(server.defaultRole.id, { SEND_MESSAGES: false, READ_MESSAGES: false, VIEW_CHANNEL: false });
+                }
+            })
+        }else{
+            message.reply("Turning off maintenance mode.");
+            lockdownChannel.delete("Maintenance mode is no longer enabled.");
+            let channels = message.guild.channels;
+            channels.forEach(c => {
+                if(c.permissionOverwrites){
+                    c.permissionOverwrites.get(server.defaultRole.id).delete();
+                }
+            })
+        }
+    }
 };
--- src/wumpi.js
+++ src/wumpi.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const {prefix, token} = require('./config.json');
-
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
@@ -15,20 +14,61 @@ for (const file of commandFiles) {
      *
      *  @param {String} command.name commands default name
      *  @param {String} command the actual command
-     *
      */
     client.commands.set(command.name, command);
 }
 
+/** Cooldown collection */
+const cooldowns = new Discord.Collection();
+
 client.on('message', message => {
     /** Actual command handler */
     if (!message.content.startsWith(prefix) || message.author.bot) return;
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-    if (!client.commands.has(command)) return;
+
+    const args = message.content.slice(prefix.length).split(' '[0]);
+    const commandName = args.shift().toLowerCase();
+
+    const command = client.commands.get(commandName)
+        || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+    if (!command) return;
+
+    if (command.guildOnly && message.channel.type !== 'text') {
+        return message.reply('I can\'t execute that command inside DMs!');
+    }
+
+    if (command.args && !args.length) {
+        let reply = `You didn't provide any arguments, ${message.author}!`;
+
+        if (command.usage) {
+            reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+        }
+
+        return message.channel.send(reply);
+    }
+
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+        }
+    }
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
     /** Command execution */
     try {
-        client.commands.get(command).execute(message, args);
+        command.execute(message, args);
     } catch (error) {
         console.error(error);
         message.reply('Error executing command!');
@@ -41,4 +81,4 @@ client.once('ready', () => {
 
 /* Bot authorization **/
 // noinspection JSIgnoredPromiseFromCall
-client.login(token);
+client.login(process.env.TOKEN);
